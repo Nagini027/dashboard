dc10a292a806a09bc663989d9f39aaea5103c759
Nagini027 <nagalakshmis027@gmail.com>
2024-08-07 00:33:17 +0530

    added responsiveness

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-
+import { FaLongArrowAltDown } from "react-icons/fa";
 import { FaBasketShopping } from "react-icons/fa6";
 import { IoMdArrowDropup } from "react-icons/io";
 import { MdOutlineArrowDropDown } from "react-icons/md";
 import { CircularProgressbar } from "react-circular-progressbar";
-
+import { BsFillHandbagFill } from "react-icons/bs";
+import { AiFillDollarCircle } from "react-icons/ai";
 function Card() {
   const value = 0.7;
   return (
@@ -12,12 +13,12 @@ function Card() {
       <div class="flex flex-wrap  px-1 mt-3">
         {/*1  */}
         <div class="p-2 max-w-48 max-h-52  ">
-          <div class=" rounded-lg  dark:bg-gray-800 bg-[#121026] border px-4 pt-3 pb-1">
+          <div class=" rounded-lg  dark:bg-gray-800 bg-[#121026] shadow px-4 pt-3 pb-1">
             <div class="  mb-3">
-              <div class="w-10 h-10 mr-3 inline-flex items-center justify-center rounded-lg dark:bg-indigo-500 bg-indigo-800 text-white flex-shrink-0">
-                <FaBasketShopping className="text-2xl" />
+              <div class="w-10 h-10 mr-3 inline-flex items-center justify-center rounded-lg bg-blue-950 dark:bg-indigo-500  text-white flex-shrink-0">
+                <FaBasketShopping className="text-2xl text-indigo-500" />
               </div>
-              <p className="text-sm font-medium text-gray-400 mt-1 ">
+              <p className="text-sm font-medium text-white mt-1 ">
                 Total Orders
               </p>
               <div className="flex justify-between ">
@@ -35,12 +36,12 @@ function Card() {
 
         {/*  2*/}
         <div class="p-2 max-w-48 max-h-52 ">
-          <div class=" rounded-lg  dark:bg-gray-800 bg-[#121026] border px-4 pt-3 pb-1">
+          <div class=" rounded-lg  dark:bg-gray-800 bg-[#121026]  px-4 pt-3 pb-1">
             <div class="  mb-3">
-              <div class="w-10 h-10 mr-3 inline-flex items-center justify-center rounded-lg dark:bg-indigo-500 bg-indigo-500 text-white flex-shrink-0">
-                <FaBasketShopping className="text-xl" />
+              <div class="w-10 h-10 mr-3 inline-flex items-center justify-center rounded-lg dark:bg-indigo-500 bg-green-900 text-white flex-shrink-0">
+                <BsFillHandbagFill className="text-2xl text-green-400" />
               </div>
-              <p className="text-sm font-medium text-gray-400 mt-1 ">
+              <p className="text-sm font-medium text-white mt-1 ">
                 Total Delivered
               </p>
               <div className="flex justify-between ">
@@ -59,12 +60,12 @@ function Card() {
 
         {/*  */}
         <div class="p-2 max-w-48 max-h-52 ">
-          <div class=" rounded-lg  dark:bg-gray-800 bg-[#121026] border px-4 pt-3 pb-1">
+          <div class=" rounded-lg  dark:bg-gray-800 bg-[#121026] px-4 pt-3 pb-1">
             <div class="  mb-3">
               <div class="w-10 h-10 mr-3 inline-flex items-center justify-center rounded-lg dark:bg-indigo-500 bg-indigo-500 text-white flex-shrink-0">
-                <FaBasketShopping className="text-xl" />
+                <BsFillHandbagFill className="text-2xl" />
               </div>
-              <p className="text-sm font-medium text-gray-400 mt-1 ">
+              <p className="text-sm font-medium text-white mt-1 ">
                 Total Cancelled
               </p>
               <div className="flex justify-between ">
@@ -81,12 +82,14 @@ function Card() {
         </div>
         {/* 4 */}
         <div class="p-2 max-w-48 max-h-52 ">
-          <div class=" rounded-lg  dark:bg-gray-800 bg-[#121026] border px-4 pt-3 pb-1">
+          <div class=" rounded-lg  dark:bg-gray-800 bg-[#121026]  px-4 pt-3 pb-1">
             <div class="  mb-3">
               <div class="w-10 h-10 mr-3 inline-flex items-center justify-center rounded-lg dark:bg-indigo-500 bg-indigo-500 text-white flex-shrink-0">
-                <FaBasketShopping className="text-xl" />
+                <AiFillDollarCircle className="text-xl mb-2 text-pink-700" />
+                <MdOutlineArrowDropDown className="-ml-4 mt-3 text-pink-500" />
+                <FaLongArrowAltDown className="-ml-1 text-pink-700" />
               </div>
-              <p className="text-sm font-medium text-gray-400 mt-1 ">
+              <p className="text-sm font-medium text-white mt-1 ">
                 Total Revenue
               </p>
               <div className="flex justify-between ">
@@ -120,7 +123,7 @@ function Card() {
                 <p>3%</p>
               </span>
             </div>
-            <div className="ml-12 p-1">
+            <div className="ml-16 p-1">
               <div style={{ width: 100, height: 100 }} className="ml-11">
                 <CircularProgressbar
                   value={value}
--- src/components/MenuCard.jsx
+++ src/components/MenuCard.jsx
@@ -7,18 +7,18 @@ import { BiDish } from "react-icons/bi";
 function MenuCard() {
   return (
     <>
-      <div className="bg-gray-900 text-white p-2 rounded-lg max-w-sm ">
+      <div className="bg-gray-900 text-white p-2 rounded-lg max-w-sm p-2">
         <div className="space-y-4 overflow-y-auto max-h-96 -ml-3 p-3">
           {/*  */}
           <div className="flex justify-between ">
             <div className="mb-3 flex">
-              <div class="w-10 h-10  inline-flex items-center justify-center rounded-full dark:bg-indigo-500 bg-indigo-500 text-white flex-shrink-0">
+              <div class="w-10 h-10  inline-flex items-center justify-center rounded-full  bg-red-900 text-red-500 flex-shrink-0">
                 <MdMyLocation />
               </div>
               <h2 className="text-white p-2">Goals</h2>
             </div>
             <div className="mt-2">
-              <div className="w-6 h-6  mr-3 inline-flex items-center justify-center rounded-full dark:bg-gray-700 bg-gray-600 text-white flex-shrink-0">
+              <div className="w-6 h-6  mr-3 inline-flex items-center justify-center rounded-full bg-gray-600 text-white flex-shrink-0">
                 <LuChevronRight />
               </div>
             </div>
@@ -26,8 +26,8 @@ function MenuCard() {
           {/*  */}
           <div className="flex justify-between ">
             <div className="mb-3 flex">
-              <div class="w-10 h-10  inline-flex items-center justify-center rounded-full dark:bg-indigo-500 bg-indigo-500 text-white flex-shrink-0">
-                <PiHamburgerFill />
+              <div class="w-10 h-10  inline-flex items-center justify-center rounded-full  bg-indigo-800 text-white flex-shrink-0">
+                <PiHamburgerFill className="text-indigo-400" />
               </div>
               <h2 className="text-white p-2">Popular Dishes</h2>
             </div>
@@ -40,10 +40,10 @@ function MenuCard() {
           {/*  */}
           <div className="flex justify-between ">
             <div className="mb-3 flex">
-              <div class="w-10 h-10  inline-flex items-center justify-center rounded-full dark:bg-indigo-500 bg-indigo-500 text-white flex-shrink-0">
-                <BiDish />
+              <div class="w-10 h-10  inline-flex items-center justify-center rounded-full  bg-cyan-900 text-white flex-shrink-0">
+                <BiDish className="text-cyan-600" />
               </div>
-              <h2 className="text-white p-2">Menus</h2>
+              <h2 className="text-white p-2 ">Menus</h2>
             </div>
             <div className="mt-2">
               <div className="w-6 h-6  mr-3 inline-flex items-center justify-center rounded-full dark:bg-gray-700 bg-gray-600 text-white flex-shrink-0">
